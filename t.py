f42d33b58723ca8add20515eba6c8d50fa8b5182
Ratmil Torres <ratmil@gmail.com>
2022-02-18 22:46:42 -0300

    Added new test

--- t.py
+++ t.py
@@ -6,3 +6,8 @@ def test_sum_numbers():
 	assert sum_numbers(1, 2) == 3
 	assert sum_numbers(2, 2) == 4
 	assert sum_numbers(1, 3) == 4
+	
+def test_sum_numbers2():
+	assert sum_numbers(1, 2) == 3
+	
+	
